173d74e3b916c2d7975b00b1c00b529674716464
Sukhpreet Singh <45624656+singh-sukhpreet@users.noreply.github.com>
2021-12-27 14:04:00 -0500

    Update common.utils.js

--- src/utilities/common.utils.js
+++ src/utilities/common.utils.js
@@ -5,30 +5,6 @@
 
 class CommonUtilities {
     
-    get btnAllow() {
-        const selectorAndroid = '//*[@text="ALLOW"] | //*[@text="Allow"]'
-        const selectorIOS = '~Allow'
-        const selector = driver.isAndroid ? selectorAndroid : selectorIOS
-        return $(selector)
-    }
-
-    get btnContinue() {
-        const selectorAndroid = '//*[@text="Continue"]'
-        const selectorIOS = '~Continue'
-        const selector = driver.isAndroid ? selectorAndroid : selectorIOS
-        return $(selector)
-    }
-
-    async click_btnAllow() {
-        await this.btnAllow.waitForDisplayed({ timeout: 2000 });
-        await this.btnAllow.click();
-    }
-
-    async click_btnContinue() {
-        await this.btnContinue.waitForDisplayed({ timeout: 2000 });
-        await this.btnContinue.click();
-    }
-
     async customAllureReporter(id=null, severity=null, issue=null) {
         if(id!=null){
             allureReporter.addTestId(id);
